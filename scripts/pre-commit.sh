3b98f421513df874197b3152227386f4fb9dda05
angrynode <angrynode@kl.netlib.re>
2025-11-07 15:11:21 +0100

    ci: Don't run tests with all features in pre-commit

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -10,7 +10,6 @@ cargo +nightly --version
 cargo +nightly fmt --check
 cargo +nightly clippy --all-features
 cargo +nightly test
-cargo +nightly test --all-features
 cargo +nightly doc --no-deps -Zrustdoc-map --all-features
 if command -v cargo-rdme 2>&1 >/dev/null; then
   cargo-rdme --check
